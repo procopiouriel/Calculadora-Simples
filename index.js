be21886e25fc79bf71d38e05a722e9307406bac1
Uriel Procópio <procopiouriel928@gmail.com>
2024-10-13 22:08:38 -0300

    Calculadora V1

---
+++ index.js
@@ -0,0 +1,8 @@
+function soma(){
+    var numero1 = document.getElementById("number1").value;
+    var numero2 = document.getElementById("number2").value;
+
+    var resultado = Number(numero1) + Number(numero2);
+
+    alert("Resultado= " + resultado)
+}
